6267eb59886f7602c78800e0cc34fb6908672db8
LeNgocNu <3123410257@sv.sgu.edu.vn>
2024-11-14 15:38:47 +0700

    khach hang

--- src/main.java
+++ src/main.java
@@ -2,28 +2,5 @@
 
 public class main {
   public static void main(String[] args) {
-    // DSNhanVien ds = new DSNhanVien();
-    // ds.addByFile("dsnv.txt");
-    // ds.them();
-    // ds.printListInFile("dsnv.txt");
-    // DSKH dskh = new DSKH();
-    // dskh.Readfile("dskh.txt");
-    // dskh.xuat();
-    // dskh.them();
-    // dskh.Writefile("dskh.txt");
-    // DoAn bur = new DoAn();
-    // bur.nhap();
-    // bur.xuat();
-    // BAN ban= new BAN();
-    // ban.nhap();
-    // ban.xuat();
-    DSBAN dsban = new DSBAN();
-    dsban.nhap();
-    dsban.xuat();
-    dsban.them();
-    dsban.xoa();
-    dsban.timkiem();
-    dsban.xuat();
-  
   }
 }
